7ddce7beb50520ca591c7a016d807f84f169689b
Saul Moreira <saul.ms@hotmail.com>
2025-04-24 12:06:33 -0300

    adicionado scaffolding spring

---
+++ src/test/java/com/smsantana/java_spring/JavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smsantana.java_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
